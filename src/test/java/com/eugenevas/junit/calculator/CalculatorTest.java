2aa99493376bf51bea72361c75c27b64796789d2
eugenevas <eugene.vas@yandex.ru>
2024-06-06 17:58:46 +0500

    Refactor code

--- src/test/java/com/eugenevas/junit/calculator/CalculatorTest.java
+++ src/test/java/com/eugenevas/junit/calculator/CalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
+class CalculatorTest {
 
     private Calculator calculator;
 
